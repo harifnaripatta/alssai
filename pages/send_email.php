eabf3d7e35af097853f59af2966e1d41baf760eb
Shafeeque <mshafeequemsn@gmail.com>
2025-04-11 10:51:38 +0200

    fixed contact form

--- pages/send_email.php
+++ pages/send_email.php
@@ -2,14 +2,16 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST["name"]);
     $email = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
     $message = htmlspecialchars($_POST["message"]);
 
     $to = "info@alssai.net"; // Replace with your email
-    $subject = "New Contact Form Submission";
+    $subject = "New Contact Form Submission: $subject";
     $headers = "From: $email\r\nReply-To: $email\r\nContent-Type: text/plain; charset=UTF-8";
 
     $body = "Name: $name\n";
     $body .= "Email: $email\n";
+    $body .= "Subject: $subject\n";
     $body .= "Message:\n$message\n";
 
     if (mail($to, $subject, $body, $headers)) {
